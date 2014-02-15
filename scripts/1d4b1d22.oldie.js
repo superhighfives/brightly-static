ab811138a4be85420aba340614be6706fdedd7e6
Charlie Gleason <hi@superhighfives.com>
2014-02-15 20:51:41 +0000

    Moved ngmin in the gruntfiles.

---
+++ scripts/1d4b1d22.oldie.js
@@ -0,0 +1,10 @@
+window.matchMedia = window.matchMedia || function (a) {
+  'use strict';
+  var b, c = a.documentElement, d = c.firstElementChild || c.firstChild, e = a.createElement('body'), f = a.createElement('div');
+  return f.id = 'mq-test-1', f.style.cssText = 'position:absolute;top:-100em', e.style.background = 'none', e.appendChild(f), function (a) {
+    return f.innerHTML = '&shy;<style media="' + a + '"> #mq-test-1 { width: 42px; }</style>', c.insertBefore(e, d), b = 42 === f.offsetWidth, c.removeChild(e), {
+      matches: b,
+      media: a
+    };
+  };
+}(document);
